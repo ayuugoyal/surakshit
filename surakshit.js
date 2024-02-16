c587c33df34b5dcf59a6293bb1d9656a7677b3a5
lunchspider <lunchspider03@gmail.com>
2024-02-17 01:40:58 +0530

    [FEAT]: added basic request blocking

--- surakshit.js
+++ surakshit.js
@@ -1,17 +1,34 @@
 // match pattern for the URLs to redirect
-let pattern = "https://developer.mozilla.org/*";
+let pattern = "<all_urls>";
+const backendUrl = "http://localhost:8000/predict";
+
+/**
+* @type {(url: string) => Promise<string>}
+**/
+async function send_request(url) {
+    try {
+        const res = await fetch(backendUrl, { method: "POST", body: JSON.stringify({ "url": url }) })
+            .then((res) => res.json()).then((j) => j.predicted_class);
+        return res;
+    } catch (e) {
+        return "safe";
+    }
+}
 
 // cancel function returns an object
 // which contains a property `cancel` set to `true`
-function cancel(requestDetails) {
-    console.log(`Canceling: ${requestDetails.url}`);
-    return { cancel: true };
+async function cancel(requestDetails) {
+    console.log(`Cancel1ing: ${requestDetails.url}`);
+    if (await send_request(requestDetails.url) != "safe") {
+        return { cancel: true };
+    }
+    return { cancel: false };
 }
 
 // add the listener,
 // passing the filter argument and "blocking"
 browser.webRequest.onBeforeRequest.addListener(
     cancel,
-    { urls: [pattern], types: ["image"] },
+    { urls: [pattern] },
     ["blocking"],
 );
